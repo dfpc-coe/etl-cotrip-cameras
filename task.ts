286222d81a7fed58930336f85befcd7bd3f9c144
ingalls <nick@ingalls.ca>
2024-10-21 08:37:27 -0600

    Initial Commit

--- task.ts
+++ task.ts
@@ -2,14 +2,8 @@ import { Type, TSchema } from '@sinclair/typebox';
 import { FeatureCollection, Feature } from 'geojson';
 import type { Event } from '@tak-ps/etl';
 import ETL, { SchemaType, handler as internal, local, env } from '@tak-ps/etl';
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars --  Fetch with an additional Response.typed(TypeBox Object) definition
 import { fetch } from '@tak-ps/etl';
 
-/**
- * The Input Schema contains the environment object that will be requested via the CloudTAK UI
- * It should be a valid TypeBox object - https://github.com/sinclairzx81/typebox
- */
 const InputSchema = Type.Object({
     'DEBUG': Type.Boolean({
         default: false,
@@ -17,10 +11,6 @@ const InputSchema = Type.Object({
     })
 });
 
-/**
- * The Output Schema contains the known properties that will be returned on the
- * GeoJSON Feature in the .properties.metdata object
- */
 const OutputSchema = Type.Object({})
 
 export default class Task extends ETL {
@@ -33,14 +23,61 @@ export default class Task extends ETL {
     }
 
     async control(): Promise<void> {
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars -- Get the Environment from the Server and ensure it conforms to the schema
-        const env = await this.env(Env);
+        await this.env(InputSchema);
+
+        const res = await fetch('https://www.cotrip.org/api/graphql', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                query: `
+                    query ($input: ListArgs!) {
+                        listCameraViewsQuery(input: $input) {
+                            cameraViews {
+                                category
+                                icon
+                                lastUpdated { timestamp timezone }
+                                title
+                                uri
+                                url
+                                sources { type src }
+                                parentCollection {
+                                    title
+                                    uri
+                                    icon
+                                    color
+                                    location { routeDesignator }
+                                    lastUpdated { timestamp timezone }
+                                }
+                            }
+                            totalRecords
+                            error { message type }
+                        }
+                    }
+                `,
+                variables: {
+                    input: {
+                        west: -180,
+                        south: -85,
+                        east: 180,
+                        north: 85,
+                        sortDirection:"DESC",
+                        sortType: "ROADWAY",
+                        freeSearchTerm:"",
+                        classificationsOrSlugs:[],
+                        recordLimit:25,
+                        recordOffset: 0
+                    }
+                }
+            })
+        })
+
+        const body = await res.json();
+
+        console.error(body);
 
         const features: Feature[] = [];
-
-        // Get things here and convert them to GeoJSON Feature Collections
-        // That conform to the node-cot Feature properties spec
-        // https://github.com/dfpc-coe/node-CoT/
 
         const fc: FeatureCollection = {
             type: 'FeatureCollection',
